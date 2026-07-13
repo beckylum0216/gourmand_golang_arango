e9a6625aace66716eccdd8e21ae6f6073bc85f91
Rebecca Lim <beckylum0214@gmail.com>
2026-07-13 20:51:02 +0800

    chore(pulling most current): pulling current

--- src/entities/ent_authentication.go
+++ src/entities/ent_authentication.go
@@ -2,7 +2,7 @@ package entities
 
 
 type Authentication struct {
-	Id       string   `json:"_id,omitempty"`
+	Id       string `json:"_id,omitempty"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
